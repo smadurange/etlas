530f265c1ba10604de4d8e6e64a73daf8768735a
Sadeep Madurange <sadeep@asciimx.com>
2024-09-01 17:37:23 +0800

    Cache data.

--- server/atlas.py
+++ server/atlas.py
@@ -3,12 +3,9 @@ from datetime import datetime, timedelta
 from threading import Lock
 
 import requests
-from passlib.hash import pbkdf2_sha256
 from flask import Flask, request, Response
 
 app = Flask(__name__)
-
-pwd = os.environ["ATLAS_KEY_HASH"]
 api_key = os.environ["POLYGON_API_KEY"]
 
 def get_tickers():
@@ -27,40 +24,42 @@ def get_tickers():
 	return tab
 
 n = 0
+ts = {}
 mtx = Lock()
 tickers = get_tickers()
+updated = datetime.now()
+update_interval = 60 * 15
 
-@app.route("/stock-prices")
+@app.route("/prices")
 def get_stock_prices():
 	global n
-
-	auth_key = request.headers.get("x-api-key")
-
-	if not pbkdf2_sha256.verify(auth_key, pwd):
-		return Response("Unauthorized\n",
-			status=401,
-			mimetype="plain/text")
-
-	base = "https://api.polygon.io/v2/aggs/ticker"
-
-	date = datetime.today()
-	e_date = date.strftime("%Y-%m-%d")
-	s_date = (date - timedelta(days=30)).strftime("%Y-%m-%d")
 
 	with mtx:
 		ticker = tickers[n]["name"]
 		price = tickers[n]["price"]
-		result = f"{ticker}\n{price}\n"
 
+		if ticker in ts and updated + timedelta(seconds=update_interval) < datetime.now():
+			print("from cache " + tick)
+			return Response(prices[ticker], status=res.status_code, mimetype="text/plain")
+
+		date = datetime.today()
+		e_date = date.strftime("%Y-%m-%d")
+		s_date = (date - timedelta(days=30)).strftime("%Y-%m-%d")
+
+		base = "https://api.polygon.io/v2/aggs/ticker"
 		url = f"{base}/{ticker}/range/1/day/{s_date}/{e_date}?adjusted=true&sort=asc&apiKey={api_key}"
-		n = (n + 1) % len(tickers)
 
 		res = requests.get(url)
+
+		result = f"{ticker}\n{price}\n"
 
 		if res.status_code == 200:
 			data = res.json()
 			for item in data["results"]:
 				result += f"{item['c']:.2f}\n"
+			ts[ticker] = result
 
-		return Response(result, status=res.status_code, mimetype="plain/text")
+			n = (n + 1) % len(tickers)
+
+		return Response(result, status=res.status_code, mimetype="text/plain")
 
